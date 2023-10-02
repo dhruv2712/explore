87c5a2b9dd0450375ea96851d646c6b3c286b450
dhruv2712 <143059886+dhruv2712@users.noreply.github.com>
2023-10-02 23:32:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+ 
+require('./app_api/models/db')
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-
+var routesApi = require('./app_api/routes/index');
 var app = express();
 
+
 // view engine setup
-app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('views', path.join(__dirname,'app_server' ,'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -21,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/api', routesApi);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,6 +41,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-var mongoose=require('mongoose');
-require('./app_server/models/db');
